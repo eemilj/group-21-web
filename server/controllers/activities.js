21ad441415009f2820b4ce8b6959fb72073ea4a8
razgoth <sramzi@net.chalmers.se>
2020-09-16 20:59:51 +0200

    added all the tests to the server folder in postman

--- server/controllers/activities.js
+++ server/controllers/activities.js
@@ -43,18 +43,26 @@ const getActivity = (req, res, next) => {
     });
 };
 
-const getActivityById = (req, res, next) => {
+
+const getActivityById = (req, res)=>{
     var id = req.params.id;
-    Activity.findById(id, function(err, activities) {
-        if (err) { return next(err); }
-        if (activities == null) {
-            return res.status(404).json(
-                {'message': 'User not found'}
-            );
-        }
-        res.json(activities);
-    });
+    Activity.findById(id)
+        .exec()
+        .then(doc=>{
+            console.log('From the database', doc);
+            if (doc){
+                res.status(200).json(doc);
+            } else{
+                res.status(404).json({message: ' no valid entry found for provided ID'});
+            }
+            res.status(200).json(doc);
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({error:err});
+        });
 };
+
 
 
 const deleteActivityById = (req, res) => {
