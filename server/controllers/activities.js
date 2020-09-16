2a24380cbc3deb1550171c5bb480b43a8faf265d
razgoth <sramzi@net.chalmers.se>
2020-09-16 12:12:30 +0200

    corrected the get id, and the patch now works in postman

--- server/controllers/activities.js
+++ server/controllers/activities.js
@@ -49,49 +49,17 @@ router.get('/api/activities', function(req, res, next){
 
 
 
-// create the body of a sub resource
-router.post('/api/activities/users', function(req, res, next){
-    var activities = new Users(req.body);
-
-    activities.save(function(err, activities) {
-        if (err) {
-            return next(err);
-        }
-        res.status(201).json(activities);
-    });
-});
-
-
-// return the sub resource
-router.get('/api/activities/users', function(req, res, next){
-    Users.find(function(err, activities){
-        if(err){
-            return next(err);
-        }
-        res.json({
-            'activities/users': activities
-        });
-    });
-});
-
-// return the document
-router.get('/api/activities/:id', function(req, res){
+router.get('/api/activities/:id', function(req, res, next) {
     var id = req.params.id;
-    Activity.findById(id)
-        .exec()
-        .then(doc=>{
-            console.log('From the database', doc);
-            if (doc){
-                res.status(200).json(doc);
-            } else{
-                res.status(404).json({message: ' no valid entry found for provided ID'});
-            }
-            res.status(200).json(doc);
-        })
-        .catch(err=>{
-            console.log(err);
-            res.status(500).json({error:err});
-        });
+    Activity.findById(id, function(err, user) {
+        if (err) { return next(err); }
+        if (user == null) {
+            return res.status(404).json(
+                {'message': 'User not found'}
+            );
+        }
+        res.json(user);
+    });
 });
 
 
