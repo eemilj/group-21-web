1d571451d8f9c216bb9b28f45c9de224cdbc65b3
razgoth <sramzi@net.chalmers.se>
2020-09-24 12:34:37 +0200

    added changes to the navbar style

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,43 @@
 <template>
+
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
+
+    <b-navbar toggleable="lg" type="light" variant="warning">
+      <b-navbar-brand href="#">Hobby Hub</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <ul class="navbar-nav ml-auto">
+
+            <li class="nav-item">
+              <a class="nav-link" href="">
+                <router-link to="/">Home</router-link>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="">
+                <router-link to="/activities">Activities</router-link>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="">
+                <router-link to="/login">Login</router-link>
+              </a>
+            </li>
+
+          </ul>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <!-- Render the content of the current page view -->
     <router-view/>
   </div>
+
 </template>
 
 <style>
@@ -14,6 +46,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+    color: #2c3e50;
 }
+
 </style>
---
+++ client/src/views/Activities.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Activities Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'activities'
+}
+</script>
+
+<style scoped>
+
+</style>
