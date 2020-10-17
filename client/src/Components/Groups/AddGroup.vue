2153abba780e9ac27344fbd14f8d77f24b0a0d4c
chrtsa <chrtsa@student.chalmers.se>
2020-10-18 02:15:58 +0300

    Prohibit the user to select an end date prior to start date when creating a group

--- client/src/Components/Groups/AddGroup.vue
+++ client/src/Components/Groups/AddGroup.vue
@@ -5,8 +5,8 @@
     <form @submit="addGroup" class="groupBox">
       <input type="text" v-model="name" name="name" placeholder="Name" class="name">
       <input type="text" v-model="location" name="location" placeholder="Location" class="location">
-      <datepicker :bootstrap-styling=true :disabledDates="disabledDates" v-model="startDate" placeholder="Start date" class="date"></datepicker>
-      <datepicker :bootstrap-styling=true :disabledDates="disabledDates" v-model="endDate" placeholder="End date" class="date"></datepicker>
+      <datepicker :bootstrap-styling=true :disabledDates="disabledStartDates" v-model="startDate" v-on:selected="disablePreviousDates" placeholder="Start date" class="date"></datepicker>
+      <datepicker :bootstrap-styling=true :disabledDates="disabledEndDates" v-model="endDate" placeholder="End date" class="date"></datepicker>
       <textarea rows="4" v-model="description" name="description" placeholder="Write description" class="description"/>
       <input type="submit" value="Submit" class="btn"/>
     </form>
@@ -27,8 +27,11 @@ export default {
       description: '',
       startDate: '',
       endDate: '',
-      disabledDates: {
+      disabledStartDates: {
         to: new Date(Date.now() - 8640000)
+      },
+      disabledEndDates: {
+        to: null
       }
     }
   },
@@ -64,6 +67,9 @@ export default {
       this.owner = ''
       this.startDate = ''
       this.endDate = ''
+    },
+    disablePreviousDates(date) {
+      this.disabledEndDates.to = date
     }
   }
 }
